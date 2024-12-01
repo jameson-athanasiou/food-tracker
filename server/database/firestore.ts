16c69313a27e21c121ab67d8a152319aa3806f98
Jameson Athanasiou <jamesonathanasiou@gmail.com>
2024-12-01 12:20:54 -0500

    database updates

--- server/database/firestore.ts
+++ server/database/firestore.ts
@@ -1,13 +1,19 @@
 import { Firestore } from '@google-cloud/firestore'
-import { AddFoodEntryInput, FoodEntry, UpdateFoodNutritionInput } from '../../types/types.generated'
+import {
+  AddFoodEntryInput,
+  DeleteFoodEntryInput,
+  FoodEntry,
+  UpdateExistingFoodEntryInput,
+  UpdateFoodNutritionInput,
+} from '../../types/types.generated'
 
 const firestore = new Firestore()
 
-const foodDetailsCollectionName = process.env.NODE_ENV === 'production' ? 'foodDetails' : 'foodDetails-test'
-const foodEntriesCollectionName = process.env.NODE_ENV === 'production' ? 'foodEntries' : 'foodEntries-test'
+const FOOD_DETAILS_COLLECTION_NAME = process.env.NODE_ENV === 'production' ? 'foodDetails' : 'foodDetails-test'
+const FOOD_ENTRIES_COLLECTION_NAME = process.env.NODE_ENV === 'production' ? 'foodEntries' : 'foodEntries-test'
 
 export const getFoodDetails = async () => {
-  const collection = firestore.collection(foodDetailsCollectionName)
+  const collection = firestore.collection(FOOD_DETAILS_COLLECTION_NAME)
   const results = await collection.get()
 
   const documents = results?.docs ?? []
@@ -19,8 +25,13 @@ export const getFoodDetails = async () => {
   return formattedDocuments
 }
 
+export const getFoodDetailsByFoodName = async (foodName: string) => {
+  const collection = await firestore.collection(FOOD_DETAILS_COLLECTION_NAME).where('food', '==', foodName).get()
+  return collection.docs.at(0)
+}
+
 export const getFoodEntriesByDate = async (date: string): Promise<Pick<FoodEntry, 'id' | 'food' | 'servings'>[]> => {
-  const collection = firestore.collection(foodEntriesCollectionName)
+  const collection = firestore.collection(FOOD_ENTRIES_COLLECTION_NAME)
   const results = await collection.where('date', '==', date).get()
 
   const documents = results?.docs ?? []
@@ -28,17 +39,39 @@ export const getFoodEntriesByDate = async (date: string): Promise<Pick<FoodEntry
 }
 
 export const addFoodEntry = async (food: AddFoodEntryInput) => {
-  const collection = firestore.collection(foodEntriesCollectionName)
+  const collection = firestore.collection(FOOD_ENTRIES_COLLECTION_NAME)
 
   const result = await collection.add(food)
 
   return food
 }
 
-export const updateFoodDetails = async (food: UpdateFoodNutritionInput) => {
-  const collection = await firestore.collection(foodDetailsCollectionName).where('food', '==', food.food).get()
+export const updateFoodEntry = async (food: UpdateExistingFoodEntryInput) => {
+  const collection = await firestore.collection(FOOD_ENTRIES_COLLECTION_NAME).where('id', '==', food.id).get()
 
   collection.forEach((doc) => doc.ref.update(food))
+
+  return food
+}
+
+export const deleteFoodEntry = async (food: DeleteFoodEntryInput) => {
+  const collection = await firestore.collection(FOOD_ENTRIES_COLLECTION_NAME).where('id', '==', food.id).get()
+
+  collection.forEach((doc) => doc.ref.delete())
+
+  return food
+}
+
+export const updateFoodDetails = async (food: UpdateFoodNutritionInput) => {
+  const collection = await firestore.collection(FOOD_DETAILS_COLLECTION_NAME).where('food', '==', food.food).get()
+
+  collection.forEach((doc) => doc.ref.update(food))
+
+  return food
+}
+
+export const addFoodDetails = async (food: string) => {
+  const collection = await firestore.collection(FOOD_DETAILS_COLLECTION_NAME).add({ food })
 
   return food
 }
--- server/resolvers/foodEntry.ts
+++ server/resolvers/foodEntry.ts
@@ -1,6 +1,14 @@
 import { FoodEntry, Resolvers } from '../../types/types.generated'
 import LocalDatabase from '../database/local'
-import { addFoodEntry, updateFoodDetails, getFoodEntriesByDate } from '../database/firestore'
+import {
+  addFoodEntry,
+  updateFoodDetails,
+  getFoodEntriesByDate,
+  updateFoodEntry,
+  deleteFoodEntry,
+  getFoodDetailsByFoodName,
+  addFoodDetails,
+} from '../database/firestore'
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -11,6 +19,12 @@ const resolvers: Resolvers = {
       const promises: Promise<unknown>[] = []
 
       promises.push(addFoodEntry({ id: input.id, food: input.food, date: input.date, servings: input.servings }))
+
+      const foodDetailsEntryExists = await getFoodDetailsByFoodName(input.food)
+
+      if (!foodDetailsEntryExists) {
+        await addFoodDetails(input.food)
+      }
 
       if (input.calcium || input.protein) {
         promises.push(
@@ -26,13 +40,16 @@ const resolvers: Resolvers = {
 
       return input
     },
-    deleteFoodEntry: (_, { input }) => {
-      LocalDatabase.deleteEntry(input)
+    deleteFoodEntry: async (_, { input }) => {
+      // LocalDatabase.deleteEntry(input)
+      await deleteFoodEntry(input)
       return { success: true, deletedId: input.id }
     },
-    updateExistingFoodEntry: (_, { input }) => {
-      LocalDatabase.updateEntry(input)
-      return LocalDatabase.getEntry({ id: input.id }) as FoodEntry
+    updateExistingFoodEntry: async (_, { input }) => {
+      // LocalDatabase.updateEntry(input)
+      // return LocalDatabase.getEntry({ id: input.id }) as FoodEntry
+      await updateFoodEntry(input)
+      return input as FoodEntry
     },
   },
   Query: {
@@ -44,11 +61,13 @@ const resolvers: Resolvers = {
     },
   },
   FoodEntry: {
-    calcium: ({ food }) => {
-      return LocalDatabase.getNutrition()[food]?.calcium || 0
+    calcium: async ({ food }) => {
+      // return LocalDatabase.getNutrition()[food]?.calcium || 0
+      return (await getFoodDetailsByFoodName(food))?.get('calcium') || 0
     },
-    protein: ({ food }) => {
-      return LocalDatabase.getNutrition()[food]?.protein || 0
+    protein: async ({ food }) => {
+      // return LocalDatabase.getNutrition()[food]?.protein || 0
+      return (await getFoodDetailsByFoodName(food))?.get('protein') || 0
     },
   },
 }
